33596ad75eb14863c7a2da0de39a6e058c40f29e
pablo alanes <pablo.alanes.dev@gmail.com>
2024-04-05 21:17:36 -0300

    refactor: Rename file

--- server.py
+++ server.py
@@ -5,18 +5,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-# Definimos un endpoint para la ruta '/datos'
-
 
 @app.route('/', methods=['GET'])
-def obtener_datos():
-    datos = {
-        'nombre': 'Juan',
-        'edad': 30,
-        'ciudad': 'Madrid'
+def example():
+    data = {
+        'name': 'Juan',
+        'age': 30,
+        'city': 'Madrid'
     }
-    return jsonify(datos)
+    return jsonify(data)
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
