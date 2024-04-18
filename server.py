bc6637c51282c81631033b4a9d672fd2203977a9
pablo alanes <pablo.alanes.dev@gmail.com>
2024-04-18 20:00:03 -0300

    fix: :bug: Add host to resolve local connection error

--- server.py
+++ server.py
@@ -23,4 +23,4 @@ def handle_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True)
