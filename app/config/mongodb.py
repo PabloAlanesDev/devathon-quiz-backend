a51884296af6617477ebd3e9d527b675f4f9305a
santiago <everardoorozcocarmona@gmail.com>
2024-04-25 14:46:58 -0500

    fixed: update enviroment mongodb

--- app/config/mongodb.py
+++ app/config/mongodb.py
@@ -3,5 +3,5 @@ import os
 mongo_config = {
     'db': os.environ.get('MONGO_DB', 'db'),
     'host': os.environ.get('MONGO_HOST', 'localhost'),
-    'port': os.environ.get('MONGO_PORT', 27017),
+    'port': int(os.environ.get('MONGO_PORT', 27017)),
 }
