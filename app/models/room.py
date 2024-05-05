c28de9ed5f5639ef39d1332f9dbe6b8c423eb28b
pablo alanes <pablo.alanes.dev@gmail.com>
2024-05-05 20:59:26 -0300

    feat: Add quiz model embed to room model

--- app/models/room.py
+++ app/models/room.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from mongoengine import StringField, IntField, EnumField, Document, EmbeddedDocumentListField
+from mongoengine import StringField, EnumField, Document, EmbeddedDocumentListField
+from app.models.room_quiz import RoomQuiz
 from app.models.room_user import RoomUser
 
 ROOM_ID_COUNT_DIGITS = 6
@@ -14,15 +15,14 @@ class RoomStatus(Enum):
 
 class Room(Document):
     id = StringField(max_length=ROOM_ID_COUNT_DIGITS, required=True, primary_key=True)
-    quiz_id = StringField(max_length=50, required=False)
-    quiz_count = IntField(required=False, default=5)
     status = EnumField(RoomStatus, default=RoomStatus.CREATED)
     users = EmbeddedDocumentListField(RoomUser)
+    quizzes = EmbeddedDocumentListField(RoomQuiz)
 
     def to_dict(self):
         return {
             'id': self.id,
-            'quiz_id': self.quiz_id,
-            'quiz_count': self.quiz_count,
-            'status': self.status.value
+            'status': self.status.value,
+            'quizzes': [q.to_dict() for q in self.quizzes],
+            'users': [u.to_dict() for u in self.users]
         }
