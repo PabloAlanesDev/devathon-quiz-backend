1bac92a90541b5ea8b07974c3f9173c26365bea4
pablo alanes <pablo.alanes.dev@gmail.com>
2024-05-05 23:19:40 -0300

    feat: Update socketio events for room

--- app/events/room.py
+++ app/events/room.py
@@ -1,64 +1,130 @@
 from flask_socketio import disconnect, join_room, emit, leave_room
 from app.events import socketio
 from app.models.room import Room, RoomStatus
-from app.models.user import User, UserStatus
-from mongoengine import ValidationError, DoesNotExist
+from mongoengine import DoesNotExist
+from flask import request
+
+from app.models.room_user import RoomUserStatus
 
 
-@socketio.on('join_room')
-def on_join(json):
+@socketio.on('connect')
+def on_connect():
+    room_id = request.args.get('room_id')
+    user_id = request.args.get('user_id')
+
+    if not room_id or not user_id:
+        disconnect()
+        return
+
+    print(f'User: {user_id} connect to Room: {room_id}\n')
+
     try:
-        # Get user from DB
-        user = User.objects.get(id=json.get('user_id'))
-
         # Get room from DB
-        room = Room.objects.get(id=json.get('room_id'), status=RoomStatus.CREATED)
+        room: Room = Room.objects.get(id=room_id, status=RoomStatus.CREATED,
+                                      users__match={'id': user_id})
 
-        # Join socket connection to room
+        # Update user status -> connect in DB
+        room.update_user_status(user_id, RoomUserStatus.CONNECT)
+
+        # Join to socket room connection
         join_room(room.id)
 
-        # Update user
-        user.update(room_id=room.id, status=UserStatus.CONNECT)
+        # Get all users from room
+        room_users = [u.to_dict() for u in room.users if u.status == RoomUserStatus.CONNECT]
 
-        # Get users of room
-        room_users = [x.to_dict() for x in User.objects(room_id=room.id)]
+        emit('message', f'User:{user_id} has joined the room', to=room.id)
 
-        emit('message', user.name + ' has entered the room.', to=room.id)
+        # Send users
         emit('room_users', room_users, to=room.id)
 
     except DoesNotExist:
-        emit('message', 'Not Found')
-        disconnect()
-    except ValidationError:
-        emit('message', 'Bad Request')
+        emit('message', f'User: {user_id} / Room: {room_id} Error: Not Found', to=room_id)
         disconnect()
     except Exception as e:
-        emit('message', str(e))
+        emit('message', f'User: {user_id} Error: {str(e)}', to=room_id)
+        disconnect()
+
+
+@socketio.on('disconnect')
+def on_disconnect():
+    room_id = request.args.get('room_id')
+    user_id = request.args.get('user_id')
+
+    if not room_id or not user_id:
+        disconnect()
+        return
+
+    print(f'User: {user_id} disconnect from Room: {room_id}\n')
+
+    try:
+        # Get room from DB
+        room = Room.objects.get(id=room_id, users__match={'id': user_id})
+
+        # Update user status -> disconnect in DB
+        room.update_user_status(user_id, RoomUserStatus.DISCONNECT)
+
+        # Leave socket connection of room
+        leave_room(room.id)
+
+        # Get users from room
+        room_users = [u.to_dict() for u in room.users if u.status != RoomUserStatus.DISCONNECT]
+
+        if room_users:
+            emit('message', f'User:{user_id} has disconnected of the room', to=room.id)
+            # Send users
+            emit('room_users', room_users, to=room.id)
+        else:
+            # Remove room
+            room.delete()
+
+        disconnect()
+
+    except DoesNotExist:
+        emit('message', f'User: {user_id} / Room: {room_id} Error: Not Found', to=room_id)
+        disconnect()
+    except Exception as e:
+        emit('message', f'User: {user_id} Error: {str(e)}', to=room_id)
+        disconnect()
 
 
 @socketio.on('leave_room')
-def on_leave(json):
+def on_leave():
+    room_id = request.args.get('room_id')
+    user_id = request.args.get('user_id')
+
+    if not room_id or not user_id:
+        disconnect()
+        return
+
+    print(f'User: {user_id} leave from Room: {room_id}\n')
+
     try:
-        # Get user from DB and update
-        user = User.objects.get(**json)
-        user.update(room_id=None, status=UserStatus.DISCONNECT)
-
         # Get room from DB
-        room = Room.objects.get(id=user.room_id)
+        room: Room = Room.objects.get(id=room_id, status=RoomStatus.FINISH,
+                                      users__match={'id': user_id})
 
-        # Leave socket connection from room
-        leave_room(user.room_id)
+        # Remove user from room
+        room.remove_user(user_id)
+
+        # Leave socket connection of room
+        leave_room(room.id)
 
         # Get users from room
-        room_users = [x.to_dict() for x in User.objects(room_id=room.id)]
+        room_users = [u.to_dict() for u in room.users if u.status != RoomUserStatus.DISCONNECT]
 
-        emit('message', user.name + ' has left the room.', to=room.id)
-        emit('room_users', room_users, to=room.id)
+        if room_users:
+            emit('message', f'User:{user_id} has leave of the room', to=room.id)
+            # Send users
+            emit('room_users', room_users, to=room.id)
+        else:
+            # Remove room
+            room.delete()
 
-        # disconnect socket connection
         disconnect()
 
     except DoesNotExist:
-        emit('message', 'Not Found')
+        emit('message', f'User: {user_id} / Room: {room_id} Error: Not Found', to=room_id)
+        disconnect()
     except Exception as e:
-        emit('message', str(e))
+        emit('message', f'User: {user_id} Error: {str(e)}', to=room_id)
+        disconnect()
--- app/models/room.py
+++ app/models/room.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from bson import ObjectId
 from mongoengine import StringField, EnumField, Document, EmbeddedDocumentListField
 from app.models.room_quiz import RoomQuiz
-from app.models.room_user import RoomUser
+from app.models.room_user import RoomUser, RoomUserStatus
 
 ROOM_ID_COUNT_DIGITS = 6
 ROOM_QUIZ_COUNT_DEFAULT = 5
@@ -12,6 +13,7 @@ class RoomStatus(Enum):
     STARTED = 'started'
     FULL = 'full'
     EMPTY = 'empty'
+    FINISH = 'finish'
 
 
 class Room(Document):
@@ -27,3 +29,22 @@ class Room(Document):
             'quizzes': [q.to_dict() for q in self.quizzes],
             'users': [u.to_dict() for u in self.users]
         }
+
+    def save(self):
+        for u in self.users:
+            if not u.id:
+                u.id = str(ObjectId())
+        for q in self.quizzes:
+            if not q.id:
+                q.id = str(ObjectId())
+        super().save()
+
+    def update_user_status(self, user_id: str, status: RoomUserStatus):
+        for user in self.users:
+            if user.id == user_id:
+                user.status = status
+        super().save()
+
+    def remove_user(self, user_id: str):
+        self.users = list(filter(lambda x: x.id != user_id, self.users))
+        super().save()
